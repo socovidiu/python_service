239ce6c5e23bde9b4305eff736b7f47c64ea7a60
socovidiu <ovidiu.suciusoc@gmail.com>
2025-07-03 18:48:17 +0300

    Wheater api

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+import requests
+import sys
+
+
 def main():
-    print("Hello from python-service!")
+
+    if len(sys.argv) == 2:
+        location = sys.argv[1]
+    else:
+        print("Usage: python main.py <location>")
+        sys.exit(1)
+    url = f"https://wttr.in/{location}"
+    response = requests.get(url)
+    print(response.text)
+    
 
 
 if __name__ == "__main__":
